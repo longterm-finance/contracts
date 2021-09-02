82a801a64e8d29bd39e6c0704cafc793d0db613f
Mihajlo Maksa <mihajlomaksa9@gmail.com>
2021-09-02 11:33:36 +0200

    add custom oracle

---
+++ contracts/oracles/CustomOracle.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol"
+
+/*
+ * THIS EXAMPLE USES THE KOVAN TESTNET 
+ */
+
+contract APIConsumer is ChainlinkClient {
+    using Chainlink for Chainlink.Request;
+
+    uint256 public volume;
+    address private oracle;
+    bytes32 private jobId;
+    uint256 private fee; 
+    
+    constructor() public {
+        setPublicChainlinkToken();
+        oracle = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8; // (Chainlink Devrel Node)
+        jobId = "d5270d1c311941d0b08bead21fea7747";
+        fee = 0.1 * 10 ** 18; // (Varies by network and job)
+    }
+    
+    /**
+     * Create a Chainlink request to retrieve API response, find the target
+     * data, then multiply by 1000000000000000000 (to remove decimal places from data).
+     */
+    function requestVolumeData() public returns (bytes32 requestId) {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        
+        // Set the URL to perform the GET request on ==> add VIX API here
+        request.add("get", "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD");
+        
+        request.add("path", "RAW.ETH.USD.VOLUME24HOUR"); // JSON path
+        
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int timesAmount = 10**18;
+        request.addInt("times", timesAmount);
+        
+        // Sends the request
+        return sendChainlinkRequestTo(oracle, request, fee);
+    }
+    
+    /**
+     * Receive the response in the form of uint256
+     */ 
+    function fulfill(bytes32 _requestId, uint256 _volume) public recordChainlinkFulfillment(_requestId) {
+        volume = _volume;
+    }
+
+    // function withdrawLink() external {} - Implement a withdraw function to avoid locking your LINK in the contract
+}
